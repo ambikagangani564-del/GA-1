1eafe2706fdc36e5f1aa5f3edbec844d27856011
ambikagangani564-del <ambikagangani564@gmail.com>
2025-11-30 17:13:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function goToPayment() {
-  window.location.href = "payment.html";
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  
-  const form = document.getElementById("paymentForm");
-  const msg = document.getElementById("msg");
-
-  if (form) {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      msg.innerHTML = "🎉 Payment Successful!";
-      msg.style.color = "green";
-    });
-  }
-
-});
